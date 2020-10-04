a430288f75afb2b70ea6b167f5e3784a668d3e8e
joansaj <joannsaj@gmail.com>
2020-10-04 23:45:23 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from 'src/app/components/about/about.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { UserComponent } from 'src/app/components/user/user.component';
+import { RepoComponent } from 'src/app/components/repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'users', component: UserComponent },
+  { path: 'repositories', component: RepoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
