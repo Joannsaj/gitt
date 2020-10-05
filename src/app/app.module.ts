cd93dae2ffebb7d6b3e020794191354ff53378e9
joansaj <joannsaj@gmail.com>
2020-10-05 07:35:14 +0300

    add highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DateCountPipe } from './date-count.pipe';
 import { RepoComponent } from './components/repo/repo.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     DateCountPipe,
     RepoComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
